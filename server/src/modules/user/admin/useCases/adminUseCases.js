02fd2ca11dd93fc724b7c84c4432b39c25c658f4
Diego B <dbsmendes@gmail.com>
2023-03-30 15:25:38 -0300

    Fix [DB] - Fix Database

--- server/src/modules/user/admin/useCases/adminUseCases.js
+++ server/src/modules/user/admin/useCases/adminUseCases.js
@@ -36,7 +36,6 @@ export class AdminUseCases extends DeleteUserUseCases {
 
     static async newArticle(data) {
         try {
-            console.log(data)
             await this.articleDbRepositories.create(data)
             return new Response(true, "Artigo criado com sucesso!")
         } catch {
