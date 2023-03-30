f0ca63c2ab73655a343bda332b80cdbe6a4dc4c5
ipami <paloma.avh@gmail.com>
2023-03-30 02:12:53 -0300

    Feat: [Home+Header+Footer] - Adjusted the layouts

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,10 +6,35 @@ import '../style/nav.css'
 import Logo from '../images/logo2.png'
 import LogoSmall from '../images/logo5.png'
 import { Link } from 'react-router-dom';
+import { useState, useEffect } from "react"
+import { AxiosProvider } from "../providers/axiosProvider"
+
 
 
 function Header(props) {
+  const hash = sessionStorage.getItem('authorization')
+  const [userInfo, setUserInfo] = useState([])
 
+
+
+const getInfo = async () => {
+  try {
+      const res = await AxiosProvider.communication('GET', 'user/informations', hash)
+
+      setUserInfo(res.data.message.name.split(' '))
+
+  } catch (err) {
+      console.log(err)
+  }
+
+
+}
+
+useEffect(() => {
+
+  getInfo();
+
+}, [])
 
 
 if(props.model == "default"){
@@ -20,7 +45,7 @@ if(props.model == "default"){
 
         <Navbar  expand="md" className="mb-3 navbar-site-field">
           <Container fluid>
-            <img src={Logo} className="logo-navbar-site"  />
+            <Link to="/"><img src={Logo} className="logo-navbar-site"  /></Link>
             <Navbar.Toggle aria-controls="offcanvasNavbar-expand-md" />
             <Navbar.Offcanvas
               id="offcanvasNavbar-expand-md"
@@ -34,10 +59,11 @@ if(props.model == "default"){
               </Offcanvas.Header>
               <Offcanvas.Body>
                 <Nav className="justify-content-end pl-4 flex-grow-1 pe-3 ">
-                <Nav.Link href="/" className='navbar-site fw-medium'>Home</Nav.Link>
-                 <Nav.Link href="/about" className='navbar-site fw-medium'>Sobre</Nav.Link>
-                 <Nav.Link href="/login" className='navbar-site fw-medium'>Funcionalidades</Nav.Link>
+                <Nav.Link href="/" className='navbar-site fw-medium'>Início</Nav.Link>
+                 <Nav.Link href="/about" className='navbar-site fw-medium'>Sobre o SYM</Nav.Link>
+                 <Nav.Link href="/ourteam" className='navbar-site fw-medium'>Nossa Equipe</Nav.Link>
                 <Nav.Link href="/contact" className='navbar-site fw-medium'>Fale Conosco</Nav.Link>
+                <Nav.Link href="/login" className='navbar-site fw-medium btn btn-outline-dark px-2'> Login</Nav.Link>
                   
                 </Nav>
               </Offcanvas.Body>
@@ -49,6 +75,50 @@ if(props.model == "default"){
     </div>
     
     )
+  }
+
+  else if(props.model == "logged" && userInfo.length != 0 ){
+
+    return (
+
+      <div >
+  
+  
+          <Navbar  expand="md" className="mb-3 navbar-site-field">
+            <Container fluid>
+            <Link to="/"> <img src={Logo} className="logo-navbar-site"  /></Link>
+              <Navbar.Toggle aria-controls="offcanvasNavbar-expand-md" />
+              <Navbar.Offcanvas
+                id="offcanvasNavbar-expand-md"
+                aria-labelledby="offcanvasNavbarLabel-expand-md"
+                placement="end"
+              >
+                <Offcanvas.Header closeButton>
+                  <Offcanvas.Title id="offcanvasNavbarLabel-expand-md">
+                     <img src={LogoSmall} style={{width:"40%" }} />
+                  </Offcanvas.Title>
+                </Offcanvas.Header>
+                <Offcanvas.Body>
+                  <Nav className="justify-content-end pl-4 flex-grow-1 pe-3 ">
+                  <Nav.Link href="/" className='navbar-site fw-medium'>Início</Nav.Link>
+                  <Nav.Link href="/about" className='navbar-site fw-medium'>Sobre o SYM</Nav.Link>
+                 <Nav.Link href="/ourteam" className='navbar-site fw-medium'>Nossa Equipe</Nav.Link>
+                  <Nav.Link href="/contact" className='navbar-site fw-medium'>Fale Conosco</Nav.Link>
+
+
+                  <Nav.Link href="/login" className='navbar-site fw-medium btn btn-outline-dark px-2'> Sair</Nav.Link>
+                    
+                  </Nav>
+                </Offcanvas.Body>
+              </Navbar.Offcanvas>
+            </Container>
+          </Navbar>
+      
+  
+      </div>
+      
+      )
+
   }
 }
 
