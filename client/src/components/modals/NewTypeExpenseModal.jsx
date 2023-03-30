dcda958db1c7146a6f2ec3c204fcc330b1e0f630
Diego B <dbsmendes@gmail.com>
2023-03-30 11:45:59 -0300

    Feat: [TypeExpense Table] - Implemented TypeExpense Table

---
+++ client/src/components/modals/NewTypeExpenseModal.jsx
@@ -0,0 +1,81 @@
+import { useState, useEffect } from 'react'
+import { AxiosProvider } from '../../providers/axiosProvider'
+import { VerticalModal } from './VerticalModal'
+import { BackdropModal } from './BackdropModal'
+
+export function NewTypeExpenseModal(props) {
+    const [feedbackUser, setFeedbackUser] = useState({
+        error: false,
+        message: ''
+    })
+    const [loadingReq, setLoadingReq] = useState(false)
+    const [values, setValues] = useState({
+        name: '',
+    })
+
+    const [isValid, setIsValid] = useState({
+        name: true,
+    })
+
+    const handleCreateTypeExpense = async () => {
+        setLoadingReq(true)
+        setFeedbackUser(prevState => ({ ...prevState, error: false, message: "" }))
+        if (!values.name) {
+            setIsValid(prevState => ({ ...prevState, name: false }))
+        }
+        else if (Object.values(isValid).every(value => value == true)) {
+            console.log('entrou')
+            try {
+                console.log('try')
+                console.log(values)
+                const response = await AxiosProvider.communication('POST', 'expenses/types/new', null, values)
+                console.log('fez req')
+                console.log(response)
+                setFeedbackUser(prevState => ({ ...prevState, message: response.data.message }))
+
+            } catch (e) {
+                console.log(e)
+                setFeedbackUser(prevState => ({ ...prevState, error: true, message: e.response.data.message }))
+            }
+        }
+        setLoadingReq(false)
+    }
+
+    useEffect(() => {
+        setValues(prevState => ({ ...prevState, name: '' }))
+        setIsValid(prevState => ({ ...prevState, name: true }))
+    }, [props.status])
+
+    return (
+        <div>
+            {feedbackUser.message && <BackdropModal
+                title={feedbackUser.error ? "Erro interno" : "Cadastro concluído"}
+                message={feedbackUser.message}
+                namebutton="Fechar"
+            />}
+
+            <VerticalModal
+                show={props.showModal}
+                onHide={props.hideModal}
+                title={'Adicionar Tipo de Despesa'}
+                namebutton={"Fechar"}
+                anotherbutton={loadingReq ? "loading" : "true"}
+                classanotherbutton={"btn table-modal-btn btn-success"}
+                anotherbuttonmessage={'Adicionar'}
+                clickanotherbutton={() => handleCreateTypeExpense()}
+                message={(
+                    <>
+                        <div className="px-3">
+                            <div className="form-floating mb-3">
+                                <input type="text" className="form-control" id="floatingInput" onFocus={() => setIsValid(prevState => ({ ...prevState, name: true }))} onChange={(event) => setValues((prevState) => ({ ...prevState, name: event.target.value }))} value={values.name} />
+                                <label htmlFor="floatingInput">Tipo de despesa</label>
+                            </div>
+                            {!isValid.name && <p className="text-danger">O campo nome não pode estar vazio.</p>}
+                        </div>
+                    </>
+                )
+                }
+            />
+        </div>
+    )
+}
--- client/src/pages/AdminDashboard.jsx
+++ client/src/pages/AdminDashboard.jsx
@@ -2,52 +2,55 @@ import React, { useState, useEffect } from 'react'
 import AdminTables from '../components/AdminTables'
 import { CreateArticleModal } from '../components/modals/CreateArticleModal'
 import '../style/admin.css'
-
+import { NewTypeExpenseModal } from '../components/modals/NewTypeExpenseModal'
+import TypeExpenseTable from '../components/TypeExpenseTable'
 
 export default function AdminDashboard() {
   const [showCreateArticleModal, setShowCreateArticleModal] = useState(false)
   const [changeInfo, setChangeInfo] = useState('')
+  const [showCreateTypeExpenseModal, setShowCreateTypeExpenseModal] = useState(false)
 
 
+  useEffect(() => {
+    console.log(changeInfo)
 
-  useEffect(() =>{
-  console.log(changeInfo)
-
-},[changeInfo])
+  }, [changeInfo])
 
   return (
     <main className="main-admin">
-      <div className= "admin-field text-center">
+      <div className="admin-field text-center">
 
         <h2 className="text-center mb-5">{`Olá, Administrador! Seja bem vindo!`}</h2>
 
         <div className="buttons-admin">
-        <button className="btn btn-admin btn-one fw-medium mt-3" onClick={() => setChangeInfo('article')}> Gerenciar artigos </button>
-        <button className="btn btn-admin btn-two fw-medium mt-3" onClick={() => setChangeInfo('user')}> Gerenciar usuários </button>
-        <button className="btn btn-admin btn-three fw-medium mt-3" onClick={() => setChangeInfo('expenses')}> Gerenciar tipos de despesas </button>
+          <button className="btn btn-admin btn-one fw-medium mt-3" onClick={() => setChangeInfo('article')}> Gerenciar artigos </button>
+          <button className="btn btn-admin btn-two fw-medium mt-3" onClick={() => setChangeInfo('user')}> Gerenciar usuários </button>
+          <button className="btn btn-admin btn-three fw-medium mt-3" onClick={() => setChangeInfo('expenses')}> Gerenciar tipos de despesas </button>
         </div>
 
-        {changeInfo == 'article'? (<>
-            <CreateArticleModal
+        {changeInfo == 'article' ? (<>
+          <CreateArticleModal
             showModal={showCreateArticleModal}
             hideModal={() => setShowCreateArticleModal(false)}
             status={showCreateArticleModal}
           />
-          <AdminTables param="allArticles"/></>) : 
-          changeInfo == 'user'? (<AdminTables param="getUsers"/>) : (
-            <div className='mt-3'></div>
-
+          <AdminTables param="allArticles" /></>) :
+          changeInfo == 'user' ? (<AdminTables param="getUsers" />) : (
+            <div className='mt-3'><TypeExpenseTable/></div>
           )}
 
-      {/* <button onClick={() => setShowCreateArticleModal(true)}>Adicionar Artigo</button> */}
+        <NewTypeExpenseModal
+          showModal={showCreateTypeExpenseModal}
+          hideModal={() => setShowCreateTypeExpenseModal(false)}
+          status={showCreateTypeExpenseModal}
+        />
 
- 
+            {/* <TypeExpenseTable/> */}
 
-
-
-
+      <button onClick={() => setShowCreateTypeExpenseModal(true)}>Abrir modal</button>
+        {/* <button onClick={() => setShowCreateArticleModal(true)}>Adicionar Artigo</button> */}
       </div>
-   </main>
+    </main>
   )
 }
 
